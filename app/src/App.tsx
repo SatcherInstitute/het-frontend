35b9116c426dc3bf373b287455c4ca38350a20b4
Chris Henrick <henrick@google.com>
2020-10-07 16:00:50 -0700

    demo of a (s)css module with App component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,23 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./App.module.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.App}>
+      <header className={styles["App-header"]}>
+        <h1>Health Equity Tracker Frontend</h1>
       </header>
     </div>
   );
