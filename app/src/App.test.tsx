773e7d772bfa6e11125ce7256c079e2c9ffe2243
Chris Henrick <henrick@google.com>
2020-10-08 15:54:34 -0700

    fixed test for App

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders the title text", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleText = getByText(/health equity tracker frontend/i);
+  expect(titleText).toBeInTheDocument();
 });
