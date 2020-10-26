b4f13693aea015191d8370d2a788a85f2e1cc84e
kkatzen <kristak@google.com>
2020-10-26 12:54:11 -0400

    Bad leaflet test

--- app/src/pages/ExploreDataPage.tsx
+++ app/src/pages/ExploreDataPage.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import VegaTest from "../features/VegaTest";
+import LeaftletTest from "../features/LeafletTest";
 
 function ExploreDataPage() {
   return (
-    <p>
+    <React.Fragment>
       Research questions; explore key relationships across datasets, chosen by
       us; explore the data freely
+      <h1>Leaflet Example</h1>
+      <LeaftletTest />
       <h1>Vega Example</h1>
       <VegaTest />
-    </p>
+    </React.Fragment>
   );
 }
 
