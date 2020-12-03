fa1026a7d85eb20c2a1dd788f886f033a3f7fef9
kkatzen <kristak@google.com>
2020-12-03 16:13:23 -0500

    PR comment pt1

--- app/src/features/reports/CompareMapNavReport.tsx
+++ app/src/features/reports/CompareMapNavReport.tsx
@@ -7,91 +7,90 @@ import useDatasetStore from "../../utils/useDatasetStore";
 import variableProviders from "../../utils/variableProviders";
 import { Breakdowns } from "../../utils/Breakdowns";
 import VariableProvider from "../../utils/variables/VariableProvider";
-import { USA_FIPS, STATE_FIPS_MAP } from "../../utils/Fips";
+import { USA_FIPS, USA_DISPLAY_NAME, STATE_FIPS_MAP } from "../../utils/Fips";
 import Alert from "@material-ui/lab/Alert";
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
 import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
 
-interface Geo {
-  fips: string;
-  name: string;
-}
+type UpdateGeoCallback = (message: string) => void;
 
 function Map(props: {
   fipsGeo: string;
   data: Record<string, any>[];
-  updateGeoCallback: Function;
+  updateGeoCallback: UpdateGeoCallback;
 }) {
-  function getStateGeoFromPropFips() {
-    return props.fipsGeo === USA_FIPS
-      ? undefined
-      : { fips: props.fipsGeo, name: STATE_FIPS_MAP[props.fipsGeo] };
-  }
-
-  const [state, setState] = useState<Geo | undefined>(
-    getStateGeoFromPropFips()
-  );
-  const [county, setCounty] = useState<Geo | undefined>();
+  const [stateFips, setStateFips] = useState<string>(props.fipsGeo);
+  const [countyFips, setCountyFips] = useState<string>();
+  const [countyName, setCountyName] = useState<string>();
 
   useEffect(() => {
-    props.updateGeoCallback(state ? state.fips : USA_FIPS);
+    props.updateGeoCallback(stateFips);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [state]);
+  }, [stateFips]);
 
   useEffect(() => {
-    setState(getStateGeoFromPropFips());
-    setCounty(undefined);
+    setStateFips(props.fipsGeo);
+    setCountyFips(undefined);
+    setCountyName(undefined);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [props.fipsGeo]);
 
   const signalListeners: any = {
     click: (...args: any) => {
-      // Is this a county or a state??
+      // Is this a countyFips or a state??
       const clickedData = args[1];
-      console.log(clickedData);
       if (clickedData.id.length === 2) {
-        setState({ fips: clickedData.id, name: clickedData.properties.name });
+        setStateFips(clickedData.id);
       } else {
-        setCounty({ fips: clickedData.id, name: clickedData.properties.name });
+        setCountyFips(clickedData.id);
+        setCountyName(clickedData.properties.name);
       }
     },
   };
 
   let dataset =
-    state === undefined
+    stateFips === USA_FIPS
       ? props.data
-      : props.data.filter((r) => r.state_fips_code === state.fips.toString());
+      : props.data.filter((r) => r.state_fips_code === stateFips);
 
+  // TODO - make the mouse turn into a pointer when you hover over
   return (
     <div>
       <Breadcrumbs aria-label="breadcrumb">
-        {state && (
+        {stateFips !== USA_FIPS && (
           <Link
             color="inherit"
             onClick={() => {
-              setState(undefined);
-              setCounty(undefined);
+              setStateFips(USA_FIPS);
+              setCountyFips(undefined);
+              setCountyName(undefined);
             }}
           >
-            United States
+            {USA_DISPLAY_NAME}
           </Link>
         )}
-        {!state && <Typography color="textPrimary">United States</Typography>}
-        {state && county && (
+        {!stateFips && (
+          <Typography color="textPrimary">{USA_DISPLAY_NAME}</Typography>
+        )}
+        {stateFips && countyFips && (
           <Link
             color="inherit"
             onClick={() => {
-              setCounty(undefined);
+              setCountyFips(undefined);
             }}
           >
-            {state.name}
+            {STATE_FIPS_MAP[stateFips]}
           </Link>
         )}
-        {state && !county && (
-          <Typography color="textPrimary">{state.name}</Typography>
+        {stateFips && !countyFips && (
+          <Typography color="textPrimary">
+            {STATE_FIPS_MAP[stateFips]}
+          </Typography>
         )}
-        {county && <Typography color="textPrimary">{county.name}</Typography>}
+        {countyFips && (
+          <Typography color="textPrimary">{countyName}</Typography>
+        )}
       </Breadcrumbs>
 
       <UsaChloroplethMap
@@ -99,16 +98,16 @@ function Map(props: {
         varField={"diabetes_count"}
         legendTitle="Diabetes Count"
         data={dataset}
-        hideLegend={state ? true : false}
-        stateFips={state ? state.fips : undefined}
-        countyFips={county ? county.fips : undefined}
+        hideLegend={stateFips ? true : false}
+        stateFips={stateFips === USA_FIPS ? undefined : stateFips}
+        countyFips={countyFips ? countyFips : undefined}
       />
-      {state !== undefined && (
+      {stateFips !== USA_FIPS && (
         <Alert severity="error">
           This dataset does not provide county level data
         </Alert>
       )}
-      {county === undefined && <TableChart data={dataset} />}
+      {countyFips === undefined && <TableChart data={dataset} />}
     </div>
   );
 }
@@ -139,14 +138,14 @@ function CompareMapNavReport(props: {
               <Map
                 data={dataset}
                 fipsGeo={props.fipsGeo1}
-                updateGeoCallback={(e: number) => props.updateGeo1Callback(e)}
+                updateGeoCallback={(e: string) => props.updateGeo1Callback(e)}
               />
             </Grid>
             <Grid item xs={6}>
               <Map
                 data={dataset}
                 fipsGeo={props.fipsGeo2}
-                updateGeoCallback={(e: number) => props.updateGeo2Callback(e)}
+                updateGeoCallback={(e: string) => props.updateGeo2Callback(e)}
               />
             </Grid>
           </Grid>
