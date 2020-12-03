45d61b16aff37b6d660839f513396da65dcccfcc
kkatzen <kristak@google.com>
2020-12-03 11:18:05 -0500

    Progress bar and UI tweaks

--- app/src/pages/LandingPage.tsx
+++ app/src/pages/LandingPage.tsx
@@ -5,8 +5,120 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { LinkWithStickyParams } from "../utils/urlutils";
 import { linkToMadLib } from "../utils/urlutils";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  image: {
+    position: "relative",
+    height: "300px",
+    width: "100%",
+    [theme.breakpoints.down("xs")]: {
+      width: "100% !important", // Overrides inline-style
+      height: 100,
+    },
+    "&:hover, &$focusVisible": {
+      zIndex: 1,
+      "& $imageBackdrop": {
+        opacity: 0.15,
+      },
+      "& $imageMarked": {
+        opacity: 0,
+      },
+      "& $imageTitle": {
+        border: "4px solid currentColor",
+      },
+    },
+  },
+  focusVisible: {},
+  imageButton: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: theme.palette.common.white,
+  },
+  imageSrc: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: "cover",
+    backgroundPosition: "center 40%",
+  },
+  imageBackdrop: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.4,
+    transition: theme.transitions.create("opacity"),
+  },
+  imageTitle: {
+    position: "relative",
+    padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${
+      theme.spacing(1) + 6
+    }px`,
+  },
+  imageMarked: {
+    height: 3,
+    width: 18,
+    backgroundColor: theme.palette.common.white,
+    position: "absolute",
+    bottom: -2,
+    left: "calc(50% - 9px)",
+    transition: theme.transitions.create("opacity"),
+  },
+}));
+
+function ImageButton(props: { imageUrl: string; text: string; link: string }) {
+  const classes = useStyles();
+
+  return (
+    <LinkWithStickyParams to={props.link}>
+      <ButtonBase
+        focusRipple
+        key={props.text}
+        className={classes.image}
+        focusVisibleClassName={classes.focusVisible}
+        style={{
+          height: "300px",
+          width: "100%",
+        }}
+      >
+        <span
+          className={classes.imageSrc}
+          style={{
+            backgroundImage: `url(${props.imageUrl})`,
+          }}
+        />
+        <span className={classes.imageBackdrop} />
+        <span className={classes.imageButton}>
+          <Typography
+            component="span"
+            variant="subtitle1"
+            color="inherit"
+            className={classes.imageTitle}
+          >
+            {props.text}
+            <span className={classes.imageMarked} />
+          </Typography>
+        </span>
+      </ButtonBase>
+    </LinkWithStickyParams>
+  );
+}
 
 function LandingPage() {
+  const classes = useStyles();
+
   return (
     <div className={styles.LandingPage}>
       <Grid container justify="space-around" className={styles.Grid}>
@@ -67,61 +179,42 @@ function LandingPage() {
           <Typography variant="h6" className={styles.HomeLogo}>
             COPD in the USA
           </Typography>
-          <p>
-            Florida has the highest cases of COPD in the united states.
-            <br />
-            <LinkWithStickyParams to={linkToMadLib("diabetes", { 1: 0 })}>
-              <Button variant="outlined">Explore Data</Button>
-            </LinkWithStickyParams>
-          </p>
+          <p>Florida has the highest cases of COPD in the united states.</p>
         </Grid>
         <Grid item xs={12} sm={6} className={styles.LandingSquare}>
-          <img
-            height="300px"
-            width="300px"
-            alt="placeholder"
-            src="img/copd_usa.png"
+          <ImageButton
+            imageUrl="img/copd_usa.png"
+            text="Explore the Data"
+            link={linkToMadLib("compare", { 3: 12 })}
           />
         </Grid>
         <Grid item xs={12} sm={6} className={styles.LandingSquare}>
-          <img
-            height="300px"
-            width="300px"
-            alt="placeholder"
-            src="img/penn_unemp.png"
+          <ImageButton
+            imageUrl="img/penn_unemp.png"
+            text="Explore the Data"
+            link={linkToMadLib("covid", { 3: 42 })}
           />
         </Grid>
         <Grid item xs={12} sm={6} className={styles.GreenLandingSquare}>
           <Typography variant="h6" className={styles.HomeLogo}>
-            Unemployment in Pennsylvania
+            COVID-19 in Pennsylvania
           </Typography>
           <p>
-            County level look at where the highest rates of unemployment are in
-            the state of Pennsylvania
-            <br />
-            <LinkWithStickyParams to={linkToMadLib("diabetes", { 1: 1 })}>
-              <Button variant="outlined">Explore Data</Button>
-            </LinkWithStickyParams>
+            Look at where the highest rates of COVID-19 are in the state of
+            Pennsylvania
           </p>
         </Grid>
         <Grid item xs={12} sm={6} className={styles.BlueLandingSquare}>
           <Typography variant="h6" className={styles.HomeLogo}>
             Diabetes in American Indian/Alaska Native, non hispanic population
           </Typography>
-          <p>
-            Explore racial breakdowns of Diabetes data in the United States
-            <br />
-            <LinkWithStickyParams to={linkToMadLib("diabetes", { 1: 1 })}>
-              <Button variant="outlined">Explore Data</Button>
-            </LinkWithStickyParams>
-          </p>
+          <p>Explore racial breakdowns of Diabetes data in the United States</p>
         </Grid>
         <Grid item xs={12} sm={6} className={styles.LandingSquare}>
-          <img
-            height="300px"
-            width="300px"
-            alt="placeholder"
-            src="img/diabetes_amin.png"
+          <ImageButton
+            imageUrl="img/diabetes_amin.png"
+            text="Explore the Data"
+            link={linkToMadLib("diabetes", { 1: 0 })}
           />
         </Grid>
         <Grid item xs={12} container className={styles.GreenLandingSquare}>
