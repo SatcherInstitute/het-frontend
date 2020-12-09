79e4bcdcf939330dbde6a6e2fc5482b9fa5d8825
kkatzen <kristak@google.com>
2020-12-09 16:02:20 -0500

    alert outside of the card

--- app/src/features/cards/TableCard.tsx
+++ app/src/features/cards/TableCard.tsx
@@ -8,18 +8,20 @@ import { Alert } from "@material-ui/lab";
 function TableCard(props: { data: Row[]; fields?: string[] }) {
   // TODO- would be nice if the header row didn't scroll with content
   return (
-    <Card raised={true} className={cardStyles.ChartCard}>
-      <div className={cardStyles.TableContainer}>
-        {props.data.length === 0 && (
-          <Alert severity="warning">
-            Missing data means that we don't know the full story.
-          </Alert>
-        )}
-        {props.data.length > 0 && (
-          <TableChart data={props.data} fields={props.fields} />
-        )}
-      </div>
-    </Card>
+    <>
+      {props.data.length === 0 && (
+        <Alert severity="warning">
+          Missing data means that we don't know the full story.
+        </Alert>
+      )}
+      {props.data.length > 0 && (
+        <Card raised={true} className={cardStyles.ChartCard}>
+          <div className={cardStyles.TableContainer}>
+            <TableChart data={props.data} fields={props.fields} />
+          </div>
+        </Card>
+      )}
+    </>
   );
 }
 
