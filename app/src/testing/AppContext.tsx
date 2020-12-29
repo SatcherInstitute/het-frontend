e2624e471ca45ad33672432186e1395ba678dc3d
Aaron <aaronsn@google.com>
2020-12-28 20:26:42 -0500

    Fix failing tests

--- app/src/testing/AppContext.tsx
+++ app/src/testing/AppContext.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import DataFetcher from "../data/DataFetcher";
 import {
   DatasetProvider,
   useDatasetStoreProvider,
 } from "../data/useDatasetStore";
+import { initGlobals } from "../utils/globals";
+import Logger from "../utils/Logger";
+
+initGlobals(new Logger(false), new DataFetcher());
 
 /** A fake app context that sets up global context for use in tests. */
 function AppContext(props: { children: React.ReactNode }) {
